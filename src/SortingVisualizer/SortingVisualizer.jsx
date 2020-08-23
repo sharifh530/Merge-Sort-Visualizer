0daf8ae73ab705e02f74d133862556ba9da798f8
Sazzad Hossain <sharifh530@gmail.com>
2020-08-24 00:50:42 +0600

    Merge sort animations implemented

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -4,27 +4,65 @@ import { getMergeSortAnimations } from "../sortingAlgorithms/mergeSortAlgo";
 
 import "./SortingVisualizer.css";
 
-const mergeSort = () => {};
+// Change this value for the speed of the animations.
+const ANIMATION_SPEED_MS = 1;
+
+// Change this value for the number of bars (value) in the array.
+const NUMBER_OF_ARRAY_BARS = 310;
+
+// This is the main color of the array bars.
+const PRIMARY_COLOR = "turquoise";
+
+// This is the color of array bars that are being compared throughout the animations.
+const SECONDARY_COLOR = "red";
+
+const mergeSort = () => {
+  const animations = getMergeSortAnimations(array);
+  for (let i = 0; i < animations.length; i++) {
+    const arrayBars = document.getElementsByClassName("array-bar");
+    const isColorChange = i % 3 !== 2;
+    if (isColorChange) {
+      const [barOneIdx, barTwoIdx] = animations[i];
+      const barOneStyle = arrayBars[barOneIdx].style;
+      const barTwoStyle = arrayBars[barTwoIdx].style;
+      const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+      setTimeout(() => {
+        barOneStyle.backgroundColor = color;
+        barTwoStyle.backgroundColor = color;
+      }, i * ANIMATION_SPEED_MS);
+    } else {
+      setTimeout(() => {
+        const [barOneIdx, newHeight] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        barOneStyle.height = `${newHeight}px`;
+      }, i * ANIMATION_SPEED_MS);
+    }
+  }
+};
 
 function SortingVisualizer() {
   const [updateArray, setUpdateArray] = useState(array);
 
   return (
-    <div className="array-container">
-      {updateArray.map((value, inx) => (
-        <div
-          className="array-bar"
-          key={inx}
-          style={{ height: `${value}px` }}
-        ></div>
-      ))}
-      <button onClick={() => window.location.reload(false)}>
-        Generate New Array
-      </button>
-      <button onClick={mergeSort}>Merge Sort</button>
-      <button>Quick Sort</button>
-      <button>Heap Sort</button>
-      <button>Bubble Sort</button>
+    <div>
+      <div>
+        <button onClick={() => window.location.reload(false)}>
+          Generate New Array
+        </button>
+        <button onClick={mergeSort}>Merge Sort</button>
+        <button>Quick Sort</button>
+        <button>Heap Sort</button>
+        <button>Bubble Sort</button>
+      </div>
+      <div className="array-container">
+        {updateArray.map((value, inx) => (
+          <div
+            className="array-bar"
+            key={inx}
+            style={{ height: `${value}px` }}
+          ></div>
+        ))}
+      </div>
     </div>
   );
 }
